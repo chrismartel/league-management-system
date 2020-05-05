a598fcda024e14d336bac58809ca913d7a7d7b9c
chrismartel <christian.martel2@mail.mcgill.ca>
2020-05-04 20:15:56 -0400

    initialized spring project

---
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/LeagueManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ca.leaguemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueManagementSystemApplication.class, args);
+	}
+
+}
---
+++ LeagueManagementSystem-Backend/src/test/java/ca/leaguemanagementsystem/LeagueManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.leaguemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
