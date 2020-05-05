34178ca2d36c3c875216e8013de2402ecfd9d3ed
chrismartel <christian.martel2@mail.mcgill.ca>
2020-05-04 20:25:57 -0400

    setup dependencies + first build following tutorial notes

--- LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/LeagueManagementSystemApplication.java
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/LeagueManagementSystemApplication.java
@@ -2,7 +2,10 @@ package ca.leaguemanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class LeagueManagementSystemApplication {
 
@@ -10,4 +13,8 @@ public class LeagueManagementSystemApplication {
 		SpringApplication.run(LeagueManagementSystemApplication.class, args);
 	}
 
+	@RequestMapping("/")
+  	public String greeting(){
+    	  return "Hello world!";
+	}
 }
