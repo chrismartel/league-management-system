63e59df8df1b60ef607f5f821672774d727964bc
Luka Loignon <lukgeneral@gmail.com>
2020-05-10 19:40:49 -0400

    Finished implementing manager request model class

--- LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/requests/ManagerRequest.java
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/requests/ManagerRequest.java
@@ -1,4 +1,38 @@
 package ca.leaguemanagementsystem.model.requests;
 
-public class ManagerRequest {
+import ca.leaguemanagementsystem.model.users.Admin;
+import ca.leaguemanagementsystem.model.users.User;
+
+import javax.persistence.*;
+
+@Inheritance(strategy = InheritanceType.JOINED)
+@Entity
+@DiscriminatorColumn(name = "manager_Request")
+public class ManagerRequest extends Request{
+
+    /* associations */
+
+    @ManyToOne
+    private Admin admin;
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    @OneToOne
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    
 }
