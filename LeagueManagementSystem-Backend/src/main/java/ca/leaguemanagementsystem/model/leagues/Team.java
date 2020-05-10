4d560f25f42ea2c900177a95bb728f492e295783
Luka Loignon <lukgeneral@gmail.com>
2020-05-10 18:48:52 -0400

    Finished implementing team model class

--- LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/Team.java
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/Team.java
@@ -1,4 +1,119 @@
 package ca.leaguemanagementsystem.model.leagues;
 
+import ca.leaguemanagementsystem.model.leagues.events.Game;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "team")
 public class Team {
+
+    @Id
+    private String teamName;
+    private String imageURL;
+    private Integer teamNumber;
+    private Integer wins;
+    private Integer losses;
+    private Integer ties;
+    private Integer pointsAgainst;
+    private Integer pointsFor;
+
+    @OneToMany(mappedBy = "myTeams")
+    private List<TeamAssignment> teamAssignments;
+
+    public List<TeamAssignment> getTeamAssignments() {
+        return teamAssignments;
+    }
+
+    public void setTeamAssignments(List<TeamAssignment> teamAssignments) {
+        this.teamAssignments = teamAssignments;
+    }
+
+    @ManyToOne
+    private Season season;
+
+    public Season getSeason() {
+        return season;
+    }
+
+    public void setSeason(Season season) {
+        this.season = season;
+    }
+
+    @ManyToOne
+    private Game game;
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    //attributes
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Integer getTeamNumber() {
+        return teamNumber;
+    }
+
+    public void setTeamNumber(Integer teamNumber) {
+        this.teamNumber = teamNumber;
+    }
+
+    public Integer getWins() {
+        return wins;
+    }
+
+    public void setWins(Integer wins) {
+        this.wins = wins;
+    }
+
+    public Integer getLosses() {
+        return losses;
+    }
+
+    public void setLosses(Integer losses) {
+        this.losses = losses;
+    }
+
+    public Integer getTies() {
+        return ties;
+    }
+
+    public void setTies(Integer ties) {
+        this.ties = ties;
+    }
+
+    public Integer getPointsAgainst() {
+        return pointsAgainst;
+    }
+
+    public void setPointsAgainst(Integer pointsAgainst) {
+        this.pointsAgainst = pointsAgainst;
+    }
+
+    public Integer getPointsFor() {
+        return pointsFor;
+    }
+
+    public void setPointsFor(Integer pointsFor) {
+        this.pointsFor = pointsFor;
+    }
 }
