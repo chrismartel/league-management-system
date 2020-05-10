a6b2b888f1ce95632448e28fe007f7e2e3907f5d
Luka Loignon <lukgeneral@gmail.com>
2020-05-10 17:06:47 -0400

    Finished implementing rulebook class for model

--- LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/RuleBook.java
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/RuleBook.java
@@ -1,4 +1,34 @@
 package ca.leaguemanagementsystem.model.leagues;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "ruleBook")
 public class RuleBook {
+    @OneToOne(mappedBy = "ruleBook")
+    private CompetitiveLeague competitiveLeague;
+
+    public CompetitiveLeague getCompetitiveLeague() {
+        return competitiveLeague;
+    }
+
+    public void setCompetitiveLeague(CompetitiveLeague competitiveLeague) {
+        this.competitiveLeague = competitiveLeague;
+    }
+
+    @OneToMany(
+            mappedBy = "ruling",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Rule> rules;
+
+    public List<Rule> getRules() {
+        return rules;
+    }
+
+    public void setRules(List<Rule> rules) {
+        this.rules = rules;
+    }
 }
