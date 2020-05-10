4460fd4dc968de6f1a03110b618d2d1fd369a9c9
Luka Loignon <lukgeneral@gmail.com>
2020-05-10 19:54:06 -0400

    Fixed location class

--- LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/ScheduleType.java
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/ScheduleType.java
@@ -1,4 +1,46 @@
 package ca.leaguemanagementsystem.model.leagues;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Inheritance(strategy = InheritanceType.JOINED)
+@Entity
+@DiscriminatorColumn(name = "schedule_type")
 public class ScheduleType {
+
+    /* associations */
+
+    /* attributes */
+    @Id
+    private String scheduleName;            // id for database
+
+    public String getScheduleName() {
+        return scheduleName;
+    }
+
+    public void setScheduleName(String scheduleName) {
+        this.scheduleName = scheduleName;
+    }
+
+    @Temporal(TemporalType.DATE)
+    private Date startDate;                 // Start date of schedule
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    @Temporal(TemporalType.DATE)
+    private Date endDate;                   // End date of schedule
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
