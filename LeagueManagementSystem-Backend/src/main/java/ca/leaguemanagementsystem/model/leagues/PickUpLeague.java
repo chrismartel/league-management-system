23bff0999582f5cd17a0b36c140637eaf708a72d
Luka Loignon <lukgeneral@gmail.com>
2020-05-10 17:36:02 -0400

    Finished pickUp league class

--- LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/PickUpLeague.java
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/PickUpLeague.java
@@ -1,4 +1,50 @@
 package ca.leaguemanagementsystem.model.leagues;
 
-public class PickUpLeague {
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@DiscriminatorValue("pcklgue")
+@Table(name = "pickUpLeague")
+public class PickUpLeague extends LeagueType{
+
+    //attributes
+    private Integer minNbPlayers;
+    private Integer maxNbPlayers;
+    private ScheduleType scheduleType;
+    private String scheduleName;
+
+    public Integer getMinNbPlayers() {
+        return minNbPlayers;
+    }
+
+    public void setMinNbPlayers(Integer minNbPlayers) {
+        this.minNbPlayers = minNbPlayers;
+    }
+
+    public Integer getMaxNbPlayers() {
+        return maxNbPlayers;
+    }
+
+    public void setMaxNbPlayers(Integer maxNbPlayers) {
+        this.maxNbPlayers = maxNbPlayers;
+    }
+
+    public ScheduleType getScheduleType() {
+        return scheduleType;
+    }
+
+    public void setScheduleType(ScheduleType scheduleType) {
+        this.scheduleType = scheduleType;
+    }
+
+    public String getScheduleName() {
+        return scheduleName;
+    }
+
+    public void setScheduleName(String scheduleName) {
+        this.scheduleName = scheduleName;
+    }
 }
+
