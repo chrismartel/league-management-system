a2b8db7084f5890452ff8a2aaf549d36fffb4671
Luka Loignon <lukgeneral@gmail.com>
2020-05-10 16:23:34 -0400

    Implemented assignment package for model

--- LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/events/assignments/Assignment.java
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/events/assignments/Assignment.java
@@ -1,4 +1,33 @@
 package ca.leaguemanagementsystem.model.leagues.events.assignments;
 
+import ca.leaguemanagementsystem.model.leagues.events.Game;
+
+import javax.persistence.*;
+
+@Inheritance(strategy = InheritanceType.JOINED)
+@Entity
+@DiscriminatorColumn(name = "assignment")
 public class Assignment {
+    @ManyToOne
+    private Game game;
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    //attributes
+
+    private boolean isAccepted;
+
+    public boolean isAccepted() {
+        return isAccepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        isAccepted = accepted;
+    }
 }
