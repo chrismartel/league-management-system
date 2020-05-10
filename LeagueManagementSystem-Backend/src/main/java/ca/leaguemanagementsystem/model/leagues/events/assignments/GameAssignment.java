745bd9cd4004156d29914f956fbc67f4a485d371
Luka Loignon <lukgeneral@gmail.com>
2020-05-10 17:30:51 -0400

    Resolved conflicts in assignment

--- LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/events/assignments/GameAssignment.java
+++ LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/events/assignments/GameAssignment.java
@@ -1,6 +1,6 @@
 package ca.leaguemanagementsystem.model.leagues.events.assignments;
 
-<<<<<<< HEAD:LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/events/assignments/Assignment.java
+
 import ca.leaguemanagementsystem.model.leagues.events.Game;
 
 import javax.persistence.*;
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Inheritance(strategy = InheritanceType.JOINED)
 @Entity
 @DiscriminatorColumn(name = "assignment")
-public class Assignment {
+public class GameAssignment {
     @ManyToOne
     private Game game;
 
@@ -31,7 +31,5 @@ public class Assignment {
     public void setAccepted(boolean accepted) {
         isAccepted = accepted;
     }
-=======
-public class GameAssignment {
->>>>>>> d9c25ae15e8a921b93e237006b1d052b6f4d0a68:LeagueManagementSystem-Backend/src/main/java/ca/leaguemanagementsystem/model/leagues/events/assignments/GameAssignment.java
+
 }
